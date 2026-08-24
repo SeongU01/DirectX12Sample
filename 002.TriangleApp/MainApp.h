6416eb1f3f71c8ae3bfa04579894d829fd62603d
SeongU <blacknaco85@gmail.com>
2026-08-24 23:03:33 +0900

    [FEATURE/002] TriangleApp 초기 프로젝트 추가
    Ultraworked with [omo](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: sisyphus-dev-ai <sisyphus-dev-ai@users.noreply.github.com>

---
+++ 002.TriangleApp/MainApp.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "pch.h"
+class Timer;
+class Application;
+class GraphicsCore;
+class ClientUI;
+class MainApp
+{
+public:
+    explicit MainApp();
+    virtual ~MainApp();
+
+public:
+    void Run();
+
+private:
+    // 애플리케이션 생성 순서를 한곳에서 관리하고 실패 여부를 호출자에게 전달한다.
+    bool Initailize(HINSTANCE hInstance);
+    // UI 입력 연결을 끊은 뒤 클라이언트, 엔진, 창, 싱글톤 순서로 정리한다.
+    void Finalize();
+
+public:
+    static MainApp* Create(HINSTANCE hInstance);
+
+private:
+    Timer*        _timer        = nullptr;
+    Application*  _application  = nullptr;
+    GraphicsCore* _graphicsCore = nullptr;
+    // 클라이언트는 위젯 상태만 소유하고 ImGui 백엔드는 GraphicsCore가 소유한다.
+    std::unique_ptr<ClientUI> _clientUI;
+};
